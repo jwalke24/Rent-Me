b5446aee7685ab3758edb20f8619829d5a7fe637
Jonah Nestrick <undecidedapollo@gmail.com>
2015-10-04 02:43:01 -0400

    Added animation to customerUC.

--- Rent Me Inventory Management Solutions/Rent Me Inventory Management Solutions/View/User Controls/CustomerUC.Designer.cs
+++ Rent Me Inventory Management Solutions/Rent Me Inventory Management Solutions/View/User Controls/CustomerUC.Designer.cs
@@ -28,53 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.enrollCustomerButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.selectCustomerButton = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
             this.saveCustomerButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
             this.ucCancelButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // enrollCustomerButton
             // 
-            this.button1.Location = new System.Drawing.Point(568, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Enroll Customer";
-            this.button1.UseVisualStyleBackColor = true;
+            this.enrollCustomerButton.Location = new System.Drawing.Point(568, 3);
+            this.enrollCustomerButton.Name = "enrollCustomerButton";
+            this.enrollCustomerButton.Size = new System.Drawing.Size(103, 23);
+            this.enrollCustomerButton.TabIndex = 0;
+            this.enrollCustomerButton.Text = "Enroll Customer";
+            this.enrollCustomerButton.UseVisualStyleBackColor = true;
+            this.enrollCustomerButton.Click += new System.EventHandler(this.enrollCustomerButton_Click);
             // 
-            // button2
+            // searchButton
             // 
-            this.button2.Location = new System.Drawing.Point(277, 107);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(91, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Search";
-            this.button2.UseVisualStyleBackColor = true;
+            this.searchButton.Location = new System.Drawing.Point(277, 107);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(91, 23);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // selectCustomerButton
             // 
-            this.button3.Enabled = false;
-            this.button3.Location = new System.Drawing.Point(568, 110);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(103, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Select Customer";
-            this.button3.UseVisualStyleBackColor = true;
+            this.selectCustomerButton.Enabled = false;
+            this.selectCustomerButton.Location = new System.Drawing.Point(568, 110);
+            this.selectCustomerButton.Name = "selectCustomerButton";
+            this.selectCustomerButton.Size = new System.Drawing.Size(103, 23);
+            this.selectCustomerButton.TabIndex = 2;
+            this.selectCustomerButton.Text = "Select Customer";
+            this.selectCustomerButton.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // searchTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(4, 109);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(267, 20);
-            this.textBox1.TabIndex = 3;
+            this.searchTextBox.Location = new System.Drawing.Point(4, 109);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(267, 20);
+            this.searchTextBox.TabIndex = 3;
             // 
             // saveCustomerButton
             // 
-            this.saveCustomerButton.Enabled = false;
             this.saveCustomerButton.Location = new System.Drawing.Point(568, 32);
             this.saveCustomerButton.Name = "saveCustomerButton";
             this.saveCustomerButton.Size = new System.Drawing.Size(103, 23);
@@ -82,10 +82,10 @@
             this.saveCustomerButton.Text = "Save Customer";
             this.saveCustomerButton.UseVisualStyleBackColor = true;
             this.saveCustomerButton.Visible = false;
+            this.saveCustomerButton.Click += new System.EventHandler(this.saveCustomerButton_Click);
             // 
             // cancelButton
             // 
-            this.cancelButton.Enabled = false;
             this.cancelButton.Location = new System.Drawing.Point(568, 61);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(103, 23);
@@ -93,6 +93,7 @@
             this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
             this.cancelButton.Visible = false;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
             // ucCancelButton
             // 
@@ -112,10 +113,10 @@
             this.Controls.Add(this.ucCancelButton);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.saveCustomerButton);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.selectCustomerButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.enrollCustomerButton);
             this.Name = "CustomerUserControl";
             this.Size = new System.Drawing.Size(674, 136);
             this.ResumeLayout(false);
@@ -125,10 +126,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button enrollCustomerButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button selectCustomerButton;
+        private System.Windows.Forms.TextBox searchTextBox;
         private System.Windows.Forms.Button saveCustomerButton;
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Button ucCancelButton;
--- Rent Me Inventory Management Solutions/Rent Me Inventory Management Solutions/View/User Controls/CustomerUC.cs
+++ Rent Me Inventory Management Solutions/Rent Me Inventory Management Solutions/View/User Controls/CustomerUC.cs
@@ -5,7 +5,8 @@ namespace Rent_Me_Inventory_Management_Solutions.View.User_Controls
 {
     internal enum CustomerStates
     {
-        Main
+        Main,
+        EnrollCustomer
     }
 
     public partial class CustomerUserControl : UserControl, IRentMeUcInterface
@@ -31,6 +32,28 @@ namespace Rent_Me_Inventory_Management_Solutions.View.User_Controls
                 this.OnStateChanged();
             }
         }
+
+        private CustomerStates internalState;
+
+        private CustomerStates InternalState
+        {
+            get { return this.internalState; }
+            set
+            {
+                this.internalState = value;
+                switch (value)
+                {
+                     case CustomerStates.EnrollCustomer:
+                        this.changeToEnrollCustomerState();
+                        break;
+                     case CustomerStates.Main:
+                        this.changeToMainState();
+                        break;
+                }
+            }
+        }
+
+
         public CustomerUserControl()
         {
             this.InitializeComponent();
@@ -43,9 +66,56 @@ namespace Rent_Me_Inventory_Management_Solutions.View.User_Controls
             this.CurrentState = RentMeUserControlPrimaryStates.Deleting;
         }
 
+
+        /// <summary>
+        /// Changes the visual state of to main.
+        /// </summary>
+        private void changeToMainState()
+        {
+            this.enrollCustomerButton.Visible = true;
+            this.selectCustomerButton.Visible = true;
+            this.ucCancelButton.Visible = true;
+            this.searchButton.Visible = true;
+            this.searchTextBox.Visible = true;
+
+            this.saveCustomerButton.Visible = false;
+            this.cancelButton.Visible = false;
+        }
+
+        /// <summary>
+        /// Changes the visual state of to enroll customer.
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        private void changeToEnrollCustomerState()
+        {
+            this.enrollCustomerButton.Visible = false;
+            this.selectCustomerButton.Visible = false;
+            this.ucCancelButton.Visible = false;
+            this.searchButton.Visible = false;
+            this.searchTextBox.Visible = false;
+
+            this.saveCustomerButton.Visible = true;
+            this.cancelButton.Visible = true;
+        }
+
         protected virtual void OnStateChanged()
         {
             this.StateChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void saveCustomerButton_Click(object sender, EventArgs e)
+        {
+            this.InternalState = CustomerStates.Main;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.InternalState = CustomerStates.Main;
+        }
+
+        private void enrollCustomerButton_Click(object sender, EventArgs e)
+        {
+            this.InternalState = CustomerStates.EnrollCustomer;
         }
     }
 }
