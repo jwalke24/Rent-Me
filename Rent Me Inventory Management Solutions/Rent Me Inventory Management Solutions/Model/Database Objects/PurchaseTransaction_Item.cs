fc7e7fa7c4453227a3025ec6717a2df0f5a1f778
Jonathan Walker <jwalke24@my.westga.edu>
2015-11-17 12:52:36 -0500

    I added code to hide the empty purchaseTransactionId column of the purchase transaction item data grid.

--- Rent Me Inventory Management Solutions/Rent Me Inventory Management Solutions/Model/Database Objects/PurchaseTransaction_Item.cs
+++ Rent Me Inventory Management Solutions/Rent Me Inventory Management Solutions/Model/Database Objects/PurchaseTransaction_Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace Rent_Me_Inventory_Management_Solutions.Model.Database_Objects
         public string FurnitureID { get; set; }
         public int Quantity { get; set; }
         public int LeaseTime { get; set; }
+
+        [Browsable(false)]
         public string PurchaseTransactionID { get; set; }
     }
 }
